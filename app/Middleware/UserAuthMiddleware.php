2b197e2aed8a970a5c9fdab77cee0d3c3125467a
visavi <visavi.net@mail.ru>
2022-02-12 01:44:17 +0300

    Поправил авторизацию по кукам

--- app/Middleware/UserAuthMiddleware.php
+++ app/Middleware/UserAuthMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Middleware;
 
 use App\Models\User;
+use App\Services\Session;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -10,20 +11,20 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 final class UserAuthMiddleware implements MiddlewareInterface
 {
-    /*public function __construct(
-        private SessionInterface $session
-    ) {}*/
+    public function __construct(
+        private Session $session
+    ) {}
 
     public function process(
         ServerRequestInterface $request,
         RequestHandlerInterface $handler
     ): ResponseInterface {
-        if (isset($_COOKIE['login'], $_COOKIE['password']) && ! isset($_SESSION['login'])) {
+        if (isset($_COOKIE['login'], $_COOKIE['password']) && ! $this->session->has('login')) {
             $user = User::query()->where('login', $_COOKIE['login'])->first();
 
             if ($user && $_COOKIE['login'] === $user->login && $_COOKIE['password'] === $user->password) {
-                $_SESSION['login'] = $user->login;
-                $_SESSION['password'] = $user->password;
+                $this->session->set('login', $user->login);
+                $this->session->set('password', $user->password);
             }
         }
 
